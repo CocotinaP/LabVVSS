f8c7848bd9d665a90053f0e67103d01a6b932757
Cosmina <cosmipopovici05@yahoo.com>
2026-02-26 17:29:03 +0200

    Surse Initiale

---
+++ src/test/java/mydrinkshop/AppTest.java
@@ -0,0 +1,13 @@
+package mydrinkshop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
