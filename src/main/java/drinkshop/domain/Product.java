14d9c0b098dd409ebc169fda4c60995972a1b7bf
grotyb <danpetri8@gmail.com>
2026-05-04 19:56:35 +0300

    Fix Java 11 compatibility issues: remove Serial annotations, replace toList() with collect(Collectors.toList()), replace isEmpty() with length()==0

--- src/main/java/drinkshop/domain/Product.java
+++ src/main/java/drinkshop/domain/Product.java
@@ -1,11 +1,9 @@
 package drinkshop.domain;
 
-import java.io.Serial;
 import java.io.Serializable;
 
 public class Product implements Serializable {
 
-    @Serial
     private static final long serialVersionUID = 1L;
     private int id;
     private String nume;
